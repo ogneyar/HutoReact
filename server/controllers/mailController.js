ae1cf4b7647d9cd395044eb6185e045c59ea50d2
ogneyar <ya13th@mail.ru>
2023-10-12 12:22:51 +0300

    auto commit

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -11,7 +11,7 @@ class mailController {
             if (!body || body.email === undefined) body = req.query
             if (!body || body.email === undefined) return next(res.json( { error: 'Отсутствуют необходимые параметры!' } ))
             let response = false
-        
+
             await mailService.send(process.env.ADMIN_EMAIL, body) 
                 .then(data => {
                     if (data.errno || data.code) console.log(JSON.stringify(data))
@@ -28,7 +28,21 @@ class mailController {
     
     async test(req, res, next) {
         try {
-            return res.json("response")
+            // return res.json("response")
+            let body = req.body
+            if (!body || body.email === undefined) body = req.query
+            if (!body || body.email === undefined) return next(res.json( { error: 'Отсутствуют необходимые параметры!' } ))
+            let response = false
+        
+            await mailService.test(process.env.ADMIN_EMAIL, body) 
+                .then(data => {
+                    if (data.errno || data.code) console.log(JSON.stringify(data))
+                    else response = true
+                    // console.log(JSON.stringify(data))
+                })
+                .catch(err => console.log(err))
+
+            return res.json(response)
         }catch(e) {
             return next(res.json( { error: 'Ошибка метода test! ' + e} ))
         }
--- server/service/mailService.js
+++ server/service/mailService.js
@@ -36,6 +36,50 @@ class MailService {
         }
     }
 
+    async test(to, data) {
+        try {
+            let response = await this.transporter.sendMail({
+                from: process.env.SMTP_USER,
+                to,
+                subject: 'Тестовое сообщение с сайта ХуторянинЪ!',
+                text: '',
+                html: `<div><h2>Сообщение от ${data.email}</h2><p>${data.message}</p></div>`,
+                attachments: [
+                    {   // utf-8 string as an attachment
+                        filename: 'text1.txt',
+                        content: 'hello world!'
+                    },
+                    {   // binary buffer as an attachment
+                        filename: 'text2.txt',
+                        content: new Buffer('hello world!','utf-8')
+                    },
+                    {   // file on disk as an attachment
+                        filename: 'text3.txt',
+                        path: 'server/service/file.txt' // stream this file
+                    },
+                    {   // use URL as an attachment
+                        filename: 'text4.txt',
+                        path: 'https://raw.github.com/nodemailer/nodemailer/master/LICENSE'
+                    },
+                    {   // encoded string as an attachment
+                        filename: 'text5.txt',
+                        content: 'aGVsbG8gd29ybGQh',
+                        encoding: 'base64'
+                    }, 
+                    {   // data uri as an attachment
+                        path: 'data:text/plain;base64,aGVsbG8gd29ybGQ='
+                    }
+                ]
+            })
+            console.log(process.env.SMTP_USER)
+            
+            return response
+        
+        }catch(e) {
+            return e
+        }
+    }
+
 }
 
 module.exports = new MailService()
