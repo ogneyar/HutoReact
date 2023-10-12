5daa7472a57fb758b052de4968f849b9a62437a2
ogneyar <ya13th@mail.ru>
2023-10-12 10:56:22 +0300

    auto commit

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -10,18 +10,21 @@ class mailController {
             let body = req.body
             if (!body || body.email === undefined) body = req.query
             if (!body || body.email === undefined) return next(res.json( { error: 'Отсутствуют необходимые параметры!' } ))
-            let response
-            await mailService.send(process.env.ADMIN_EMAIL, body) 
-                .then(data => {
-                    // data = JSON.parse(JSON.stringify(data))
-                    if (data.errno || data.code) response = false
-                    else response = true
-                    console.log(JSON.stringify(data))
-                })
-                .catch(err => {
-                    response = false
-                    // console.log(err)
-                })
+            let response = undefined
+            // await mailService.send(process.env.ADMIN_EMAIL, body) 
+            //     .then(data => {
+            //         // data = JSON.parse(JSON.stringify(data))
+            //         if (data.errno || data.code) response = false
+            //         else response = true
+            //         console.log(JSON.stringify(data))
+            //     })
+            //     .catch(err => {
+            //         response = false
+            //         // console.log(err)
+            //     })
+            //     .finally(_ => {
+            //         if (response === undefined) response = false
+            //     })
 
             return res.json(response)
         }catch(e) {
