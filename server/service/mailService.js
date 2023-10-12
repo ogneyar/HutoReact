81e259d3b9555738a73c972c57c1b9acb57b6b2d
ogneyar <ya13th@mail.ru>
2023-10-12 10:01:41 +0300

    auto commit

--- server/service/mailService.js
+++ server/service/mailService.js
@@ -6,10 +6,10 @@ class MailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT_SECURE,
-            // port: process.env.SMTP_PORT,
+            // port: process.env.SMTP_PORT_SECURE,
+            port: process.env.SMTP_PORT,
             // port: process.env.SMTP_PORT_2,
-            secure: true,
+            // secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
