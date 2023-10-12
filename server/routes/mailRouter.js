dc470e23c3e532bf39c3c7edd56f947597c71ba9
ogneyar <ya13th@mail.ru>
2023-10-12 08:57:17 +0300

    auto commit

--- server/routes/mailRouter.js
+++ server/routes/mailRouter.js
@@ -5,9 +5,9 @@ const router = new Router()
 const mailController = require('../controllers/mailController')
 
 if (process.env.URL_API === "http://localhost:5000") {
-    router.get('/support', mailController.support) 
     router.get('/test', mailController.test) 
 }
+router.get('/support', mailController.support) 
 router.post('/support', mailController.support) 
 
 module.exports = router
